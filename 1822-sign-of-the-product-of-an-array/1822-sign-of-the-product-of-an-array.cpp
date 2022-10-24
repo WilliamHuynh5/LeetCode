0e64db0d7d5b5460953110759ef988c5677e5669
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2022-10-24 11:36:03 +1100

    Time: 22 ms (6.82%), Space: 10.3 MB (44.10%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        if (find(nums.begin(), nums.end(), 0) != nums.end()){
-            return 0;
-        }
         
         auto sign = 1;
         for (auto num : nums) {
+            if (num == 0) return 0;
             (num < 0) ? sign *= -1 : sign *= 1;
         }
         return sign;
