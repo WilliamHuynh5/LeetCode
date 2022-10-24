9dcf7e6460f5272f325d96f6ced5265103d73e05
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2022-10-24 12:10:48 +1100

    Time: 4 ms (42.56%), Space: 6.2 MB (62.43%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        if (s1 == s2) return true;
+        if (s1.size() == 1 || s1.size() == 2) return false;
+        auto numDiff = 0;
+        vector<int> vec;
+        for (auto i = 0; i < s1.size(); i++) {
+            if (s1[i] == s2[i]) continue;
+            if (++numDiff > 2) return false;
+            
+            vec.push_back(i);
+        }
+        
+        if (numDiff == 1) return false;
+        
+        auto charA = s1.at(vec[0]);
+        auto charB = s1.at(vec[1]);
+        
+        s1.at(vec[0]) = charB;
+        s1.at(vec[1]) = charA;
+        
+        return s1 == s2;
+    }
+};
