61dd97c37a2fd582760e80fcab16ab2dc665d604
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2022-10-26 22:10:44 +1100

    Time: 15 ms (76.22%), Space: 11.5 MB (5.02%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        auto x = mat[0].size();
+        auto y = mat.size();
+        
+        vector<int> merged;
+        
+        for (auto subArr : mat) {
+            for (auto elem : subArr) {
+                merged.push_back(elem);
+            }
+        }
+        
+        if (r * c != merged.size()) {
+            return mat;
+        }
+        
+        vector<vector<int>> res;
+        auto count = 0;
+        vector<int> temp;
+        
+        auto i = 0;
+        while (i < merged.size()) {
+            if (count < c) {
+                temp.push_back(merged[i]);
+                i++;
+            } 
+            if (count == c || c == 1){
+                res.push_back(temp);
+                temp.clear();
+                count = 0;
+                continue;
+            }
+            count++;
+            
+        }
+        
+//         for (auto elem : merged) {
+//             cout << count << "\n";
+            
+//             if (count < c) {
+//                 temp.push_back(elem);
+//             } 
+//             if (count == c || c == 1){
+//                 res.push_back(temp);
+//                 temp.clear();
+//                 temp.push_back(elem);
+//                 count = 0;
+//                 continue;
+//             }
+//             count++;
+//         }
+        if (c != 1) { 
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
