2e1ad46265b305f7684f3da0681823e48d85c206
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2022-10-27 16:48:25 +1100

    Time: 0 ms (100.00%), Space: 6.4 MB (17.69%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        
+        auto length = (word1.size() > word2.size()) ? word1.size() : word2.size();
+        string res = "";
+        
+        for (auto i = 0; i < length; i++) {
+            if (i < word1.size()) { res+=word1[i]; }
+            if (i < word2.size()) { res+=word2[i]; }
+        }
+        
+        return res;
+    }
+};
