0ad1c3d86c2b69b17244a6498e8b488d7e2b3134
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-02-13 16:47:15 +1100

    Time: 0 ms (100.00%), Space: 39.5 MB (19.39%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        for (int i = n; i < Integer.MAX_VALUE; i++) {
+            if (i % 2 == 0 && i % n == 0) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
