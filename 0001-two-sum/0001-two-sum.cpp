5c34b4dc9b3034dd9fe16df5248bb3538de94032
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-03-04 12:24:43 +1100

    Time: 166 ms (49.65%), Space: 10.3 MB (73.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        
         vector<int> res;
+    
         for (auto i = 0; i < nums.size(); i++) {
-            auto query = target - nums.at(i);
-            cout << query << "\n";
-            auto out = find(nums.begin(), nums.end(), query);
-            if (out != nums.end()) {
-                auto index = out - nums.begin();
-                if (i == index) {
+            auto search = target - nums.at(i);
+            cout << nums.size() << "\n";
+            auto f = find(nums.begin(), nums.end(), search);
+            if (f != nums.end()) {
+                if (f - nums.begin() == i) {
+                    res.clear();
                     continue;
                 }
                 res.push_back(i);
-                res.push_back(index);
+                res.push_back(f - nums.begin());
                 break;
             }
+            res.clear();
         }
         return res;
     }
