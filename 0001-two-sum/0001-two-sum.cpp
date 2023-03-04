dba9653902e118edae9cd67f19ef8cc98ea97e11
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-03-04 12:31:25 +1100

    Time: 4 ms (99.18%), Space: 11.3 MB (13.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        vector<int> res;
     
+        map<int, int> m;
+        
         for (auto i = 0; i < nums.size(); i++) {
-            auto search = target - nums.at(i);
-            auto f = find(nums.begin(), nums.end(), search);
-            if (f != nums.end()) {
-                if (f - nums.begin() == i) {
-                    continue;
-                }
-                res.push_back(i);
-                res.push_back(f - nums.begin());
-                break;
+            auto elem = nums.at(i);
+            auto search = target - elem;
+            if (m.count(search)) {
+                return {m[search], i};
+            } else {
+                m[elem] = i;
             }
-            res.clear();
         }
-        return res;
+        return {0, 0};
     }
 };
+    
+/*
+    vector<int> res;
+
+    for (auto i = 0; i < nums.size(); i++) {
+        auto search = target - nums.at(i);
+        auto f = find(nums.begin(), nums.end(), search);
+        if (f != nums.end()) {
+            if (f - nums.begin() == i) {
+                continue;
+            }
+            res.push_back(i);
+            res.push_back(f - nums.begin());
+            break;
+        }
+        res.clear();
+    }
+    return res;
+*/
