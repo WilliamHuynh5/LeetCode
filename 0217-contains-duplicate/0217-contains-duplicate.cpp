a1fdee961429d368eb012ee76ebea9efe10ed9a5
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-03-04 12:02:42 +1100

    Time: 194 ms (22.74%), Space: 70.8 MB (20.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // Sorting the vector. Will take O(nlogn) time as it uses a merge sort
-        sort(nums.begin(), nums.end());
+
+        map<int, int> m;
         
-        // Iterate through the vector and keep a copy of the previous element. 
-        // If the previous element matches the current element, there is a duplicate.
-        // Runs in O(n) time
-        
-        for (auto i = 1; i < nums.size(); i++) {
-            if (nums.at(i - 1) == nums.at(i)) {
+        for (auto elem : nums) {
+            if (m.find(elem) != m.end()) {
                 return true;
+            } else {
+                m[elem] = 1;
             }
         }
         return false;
     }
 };
+
+/*
+    // Sorting the vector. Will take O(nlogn) time as it uses a merge sort
+    sort(nums.begin(), nums.end());
+
+    // Iterate through the vector and keep a copy of the previous element. 
+    // If the previous element matches the current element, there is a duplicate.
+    // Runs in O(n) time
+
+    for (auto i = 1; i < nums.size(); i++) {
+        if (nums.at(i - 1) == nums.at(i)) {
+            return true;
+        }
+    }
+    return false;
+*/
