27b3ab771767cc32948148f81efb35fe15060cc1
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-03-04 12:25:05 +1100

    Time: 146 ms (49.87%), Space: 10.3 MB (73.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,16 +6,14 @@ public:
     
         for (auto i = 0; i < nums.size(); i++) {
             auto search = target - nums.at(i);
-            cout << nums.size() << "\n";
             auto f = find(nums.begin(), nums.end(), search);
             if (f != nums.end()) {
                 if (f - nums.begin() == i) {
-                    res.clear();
                     continue;
                 }
                 res.push_back(i);
                 res.push_back(f - nums.begin());
-                break;
+                return res;
             }
             res.clear();
         }
