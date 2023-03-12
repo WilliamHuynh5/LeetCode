61d5ea75023eb0e68d81abec48b4cf308fce31f5
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-03-12 20:27:00 +1100

    Time: 0 ms (100.00%), Space: 6.5 MB (11.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,22 +3,23 @@ public:
     bool isValid(string s) {
         stack<char> stk;
         
+        map<char, char> mp;
+        
+        mp['('] = ')';
+        mp['['] = ']';
+        mp['{'] = '}';
+        
         for (auto elem : s) {
-            if (elem == '(' || elem == '[' || elem == '{'){
-                stk.push(elem);
-            } else {
-                auto top = stk.top();
-                
-                if (stk.empty() || (top == '(' && elem != ')') || (top == '[' && elem != ']') || (top == '{' && elem != '}')) {
-                    return false;
-                }
+            if (stk.size() > 0 && elem == mp[stk.top()]) {
                 stk.pop();
+            } else {
+                stk.push(elem);
             }
         }
-        
-        if (stk.empty()){
-            return true;
-        }
-        return false;
+
+        if (stk.size() > 0) {
+            return false;
+        } 
+        return true;
     }
 };
