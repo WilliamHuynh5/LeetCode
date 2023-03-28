a5420dd10f7cbe429a825ce75f995598d37a7da8
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-03-28 11:34:26 +1100

    Time: 0 ms (100.00%), Space: 8.8 MB (47.56%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        auto k = 0;
+        for (auto i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k+=1;
+            }
+        }
+        return k;
+    }
+};
