6ed753c15a30ced9e6c43323de940c4577fd3b80
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-04-30 12:31:49 +1000

    Time: 24 ms (20.27%), Space: 4.5 MB (24.70%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.rs
+++ 0217-contains-duplicate/0217-contains-duplicate.rs
@@ -1,11 +1,15 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        let mut mut_nums = nums.clone();
-        mut_nums.sort();
-        for idx in 0..mut_nums.len() - 1 {
-            if mut_nums[idx] == mut_nums[idx + 1] {
+        let mut map = HashMap::new();
+
+        for num in nums {
+            if map.contains_key(&num) {
                 return true;
-            };
+            } else {
+                map.insert(num, 1);
+            }
         }
         false
     }
