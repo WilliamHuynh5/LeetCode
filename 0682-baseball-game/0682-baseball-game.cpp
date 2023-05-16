1b0400819eb4cea1a70c0023719f9551e60d3692
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-05-16 20:15:54 +1000

    Time: 12 ms (13.10%), Space: 8.2 MB (99.96%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> stack;
+        
+        for (auto i = 0; i < operations.size(); i++) {
+            auto op = operations[i];
+            cout << op << "\n";
+            if (op == "+") {
+                auto score = stack.back() + stack[stack.size() - 2];
+                stack.push_back(score);
+            } else if (op == "D") {
+                auto score = stack.back() * 2;
+                stack.push_back(score);
+            } else if (op == "C") {
+                stack.pop_back();
+            } else {
+                stack.push_back(stoi(op));
+            }
+        }
+        
+        return accumulate(stack.begin(), stack.end(), 0);
+    }
+};
