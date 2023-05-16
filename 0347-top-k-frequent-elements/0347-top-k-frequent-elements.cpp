ac9efaff98a5b8910827eacecb8e6450b83e4426
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-05-16 12:08:07 +1000

    Time: 20 ms (41.12%), Space: 15.1 MB (12.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
  
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        // Initialise frequency table 
         map<int, int> freq;
         for (auto elem : nums) {
             freq[elem]++;
         }
         
+        // Initialise bucket
         vector<int> bucket[nums.size() + 1];
         for (auto i = 0; i < nums.size(); i++) {
             vector<int> v;
             bucket[i] = v;
         }
 
+        // Sort into the bucket
         auto max = -1;
         for (auto elem : freq) {   
             bucket[elem.second].push_back(elem.first);
@@ -21,8 +25,8 @@ public:
             }
         } 
         
+        // Get the top K elements
         vector<int> res;
-        
         while (res.size() != k) {
             res.insert(res.end(), bucket[max].begin(), bucket[max].end());
             max--;
