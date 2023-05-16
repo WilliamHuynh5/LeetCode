eacf700a8f745fdcb46807693e35e3368c8c85a4
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-05-16 12:05:21 +1000

    Time: 235 ms (40.98%), Space: 68.9 MB (5.03%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,25 +2,33 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         
+        // If size is 0, return 0
         if (nums.size() == 0) return 0;
         
+        // Initialise the map where the keys are elements in nums
         map<int, int> m;
-        
         for (auto num : nums) {
             m[num] = 1;
         }
         
+        // Two pointers technique, compare the prev with the current key
         auto prev = (*m.begin()).first;
+        
+        // current streak and longest streak
         auto current = 1;
         auto longest = 1;
         
+        // Iterate through keys in the map
         for (auto pair : m) {
+            // Use two pointers technique to keep track of current key and previous key
+            // If key differs by 1, then increment current
             if (prev + 1 == pair.first) {
                 current++;
                 longest = max(current, longest);
             } else {
                 current = 1;
             }
+            // Keep track of the previous key
             prev = pair.first;
         }
         return longest;
