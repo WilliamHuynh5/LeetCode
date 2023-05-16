07e072ff0761e9614d244ff893f066df215ffce2
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-05-16 20:19:25 +1000

    Time: 8 ms (28.99%), Space: 8.3 MB (93.66%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int calPoints(vector<string>& operations) {
+        
+        // Uses a stack to store the score records
         vector<int> stack;
         
-        for (auto i = 0; i < operations.size(); i++) {
-            auto op = operations[i];
+        // Iterate through all the operations
+        for (auto op : operations) {
             if (op == "+") {
+                // Sum the last and second last value in the stack
                 auto score = stack.back() + stack[stack.size() - 2];
                 stack.push_back(score);
             } else if (op == "D") {
+                // Double the last value in the stack
                 auto score = stack.back() * 2;
                 stack.push_back(score);
             } else if (op == "C") {
+                // Remove the last value in the stack
                 stack.pop_back();
             } else {
                 stack.push_back(stoi(op));
             }
         }
         
+        // Sum of the stack
         return accumulate(stack.begin(), stack.end(), 0);
     }
 };
