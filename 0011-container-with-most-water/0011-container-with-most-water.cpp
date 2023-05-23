5927958584052f6c166317e31ec2aac1e9896e90
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-05-23 12:04:57 +1000

    Time: 106 ms (37.03%), Space: 59.2 MB (6.56%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        
+        // Intialise right pointer, left pointer and current max
         auto lp = 0;
         auto rp = height.size() - 1;
         auto curr_max = 0;
         
+        // Whilst the lp < rp
         while (lp < rp) {
             
+            // Calculate area
             auto area = min(height[lp], height[rp]) * (rp - lp);
             
+            // Set the current max
             if (area > curr_max) {
                 curr_max = area;
             }
             
+            // Follow a greedy approach by trying to maximise the value of the lp and rp
+            // Move the pointer depending on whether the lp value is less than the rp value and vice versa
             if (height[lp] < height[rp]) {
                 lp++;
             } else {
