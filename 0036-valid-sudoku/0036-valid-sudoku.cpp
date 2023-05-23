3be40e818331e123a587adb2bee53a67e8fe8b3f
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-05-23 11:42:53 +1000

    Time: 23 ms (67.75%), Space: 20.4 MB (20.83%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // check row
-        // check col
-        // check box
         
-        // Create sets, as every element must be unique
+        // Create vec of vecs
         vector<vector<char>> row;
         vector<vector<char>> col;
         vector<vector<char>> box;
         
+        // Push sub vecs into vecs
         for (auto i = 0; i < 9; i++) {
             vector<char> subrow;
             vector<char> subcol;
@@ -20,24 +18,30 @@ public:
             box.push_back(subbox);
         }
         
+        // Iterate through the sudoku board
         for (auto i = 0; i < 9; i++) {
             for (auto j = 0; j < 9; j++) {
+                
+                // If empty, skip
                 if (board[i][j] == '.') {
                     continue;
                 }
                 
+                // Check if exists in row, if does - return false, otherwise push to row vec
                 if (std::count(row[i].begin(), row[i].end(), board[i][j])) {
                     return false;
                 } else {
                     row[i].push_back(board[i][j]);
                 }
                 
+                // Check if exists in col, if does - return false, otherwise push to col vec
                 if (std::count(col[j].begin(), col[j].end(), board[i][j])) {
                     return false;
                 } else {
                     col[j].push_back(board[i][j]);
                 }
                 
+                // Check if exists in box, if does - return false, otherwise push to box vec using math magic
                 if (std::count(box[(j / 3)* 3 + i / 3].begin(), box[(j / 3)* 3 + i / 3].end(), board[i][j])) {
                     return false;
                 } else {
