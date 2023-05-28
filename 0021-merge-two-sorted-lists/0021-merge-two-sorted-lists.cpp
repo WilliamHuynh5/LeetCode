5babca1ed21ee1342a6f0e33866cdcfc5cbd1964
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-05-28 11:21:40 +1000

    Time: 8 ms (69.45%), Space: 14.7 MB (71.99%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -12,29 +12,41 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
+        // Create a new List
         ListNode res = ListNode();
+        // Pointer to that new list
         ListNode *temp = &res;
         
+        // Iterate through both list1 and list2
         while (list1 && list2) {
             
+            // If list1 node is less than list2 node, 
+            // insert it into the temp list
+            // move the list1 pointer next
             if (list1->val < list2->val) {
                 temp->next = list1;
                 list1 = list1->next;
-            } else {
+            } 
+            // Otherwise, insert the list2 node into the temp list
+            // move the list2 pointer next
+            else {
                 temp->next = list2;
                 list2 = list2->next;
             }
+            // Move the temp list pointer next
             temp = temp->next;
         }
-        // cout << temp->val << "\n";
-        // cout << list1->val << "\n";
-        // cout << list2->val << "\n";
+        
+        // There will be leftover elements in one of the lists
+        // Since those elements are already sorted,
+        // simply append whichever list is not NULLPTR at the end of temp list
         if (list1 == nullptr) {
             temp->next = list2;
         } else if (list2 == nullptr) {
             temp->next = list1;
         }
         
+        // Return the list
         return res.next;
     }
 };
