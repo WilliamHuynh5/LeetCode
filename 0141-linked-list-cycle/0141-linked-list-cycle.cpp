ef9316afda1d3d7a7378e5023453950379f0db79
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-05-28 12:30:37 +1000

    Time: 15 ms (28.94%), Space: 8 MB (74.97%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,21 +9,33 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (head == NULL) {
+        // Use Floyd's tortoise and hare algorithm
+        // It is a cycle-finding algorithm that uses two pointers,
+        // a slow pointer (tortoise) and a fast pointer (hare)
+        // the slow pointer is incremented by 1 position
+        // the fast pointer is incremented by 2 positions
+        
+        // If there is NO CYCLE, the fast pointer will encounter `nullptr`, and hence can return false
+        // If there is A CYCLE, the fast pointer and the slow pointer
+        // will eventually intersect, and once they do, we can return true
+        
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        
+        if  (head == nullptr) {
             return false;
         }
         
-        ListNode* slow = head;
-        ListNode* fast = head;
+        ListNode *slow = head;
+        ListNode *fast = head;
         
-        while (fast->next != NULL && fast->next->next != NULL) {
+        while (fast->next && fast->next->next) {
             slow = slow->next;
             fast = fast->next->next;
             if (slow == fast) {
                 return true;
             }
         }
-        
         return false;
     }
 };
