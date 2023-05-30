6fc6122cb55383a90e08e900229727d1e83a0cf9
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-05-30 21:34:04 +1000

    Time: 42 ms (72.47%), Space: 19.7 MB (29.21%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.cpp
+++ 0707-design-linked-list/0707-design-linked-list.cpp
@@ -1,10 +1,13 @@
 #include <cstdlib>
+
+// Struct for a Node
 typedef struct Node {
     int val;
     Node *next;
 } Node;
 
 class MyLinkedList {
+    // Singly linked list
     Node *head;
     Node *tail;
     int size;
@@ -17,39 +20,54 @@ public:
     }
     
     int get(int index) {
+        // If out of bounds, return -1
         if (index  < 0 || index >= size) {
             return -1;
         } 
-        
+    
+        // Create a current node and traverse to the index
         struct Node* curr = head;
         for (auto i = 0; i < index; i++) {
             curr = curr->next;
         }
+        // Return value of node at index
         return curr->val;
     }
     
     void addAtHead(int val) {
+        // Create a new node
         struct Node* n = new Node;
         n->val = val;
         
+        // Since, n will be the new head,
+        // make n's next point to the old head
         n->next = head;
+        // If size is 1, then the new tail will be the old head
         if (size == 1) {
             tail = head;
         }
+        // Make the new head point to n
         head = n;
         size++;
     }
     
     void addAtTail(int val) {
+        // Create a new node
         struct Node* n = new Node;
         n->val = val; 
         
+        // If size is 0, then the new head will be n
         if(size == 0) {
             head = n;
         }
-        
+        // Since inserting at tail,
+        // simply make the tail->next point to the new node
         tail->next = n;
+        
+        // Increment the tail to the new node
         tail = tail->next;
+        
+        // If size of list was 1, make the head->next point to the new tail
         if (size == 1) {
             head->next = tail;
         }
@@ -58,46 +76,69 @@ public:
     }
     
     void addAtIndex(int index, int val) {
+        // If out of bounds, return 
         if (index  < 0 || index > size) {
             return;
-        } else if (index == 0) {
+        } 
+        // If index is 0, add at head
+        else if (index == 0) {
             addAtHead(val);
-        } else if (index == size) {
+        } 
+        // If index is size, add at tail
+        else if (index == size) {
             addAtTail(val);
-        } else {    
+        } 
+        // Else, traverse to the index and insert
+        else {    
             struct Node* n = new Node;
             n->val = val;
-
+            
+            // Traverse to the index
             struct Node* curr = head;
             for (auto i = 0; i < index - 1;  i++) {
                 curr = curr->next;
             }
+            
+            // Keep a temp pointer to the next node
             struct Node* temp = curr->next;
+            // Set the next pointer of the current node to the new node
             curr->next = n;
+            // Set the next pointer of the new node to the temp pointer
             n->next = temp;  
             size++;    
         }
     }
     
     void deleteAtIndex(int index) {
+        // If out of bounds, return
         if (index < 0 || index >= size) {
             return;
-        } else if (index == 0) {
+        } 
+        // If index is 0, simply increment the head
+        else if (index == 0) {
             head = head->next;
             size--;
-        } else if (index == size - 1) {
+        } 
+        // If index is size - 1, make the tail point to the tail's previous node
+        else if (index == size - 1) {
             struct Node* curr = head;
+            // Traverse to the node before the tail
             for (auto i = 0; i < index - 1;  i++) {
                 curr = curr->next;
             }
+            // Set the next pointer of the node before the tail to NULL
             curr->next = NULL;
             tail = curr;
             size--;
-        } else {
+        } 
+        // Else, traverse to the index and delete
+        else {
             struct Node* curr = head;
+            // Traverse to the node before the index
             for (auto i = 0; i < index - 1;  i++) {
                 curr = curr->next;
             }
+            // Set the next pointer of the node before the index to the node after the index
             curr->next = curr->next->next;
             size--;
         }
