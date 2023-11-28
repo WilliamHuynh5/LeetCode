ff66bf055afc422d4ae608bb51d021099e2d6631
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-11-29 09:37:45 +1100

    Time: 64 ms (22.85%), Space: 44.3 MB (83.50%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
@@ -0,0 +1,14 @@
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+    if (nums.length === 0) {
+        return init;
+    }
+
+
+    for (const elem of nums) {
+        init = (fn(init, elem));
+    }
+
+    return init;
+};
