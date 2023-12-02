cf2513c2e06268908122e726a0676a1936d01844
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-12-02 12:36:15 +1100

    Time: 57 ms (45.92%), Space: 43.2 MB (38.80%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.ts
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.ts
@@ -3,9 +3,9 @@ type Fn = (n: number, i: number) => any
 function filter(arr: number[], fn: Fn): number[] {
 	let res: number[] = [];
     
-    for (const index in arr) {
-        if (fn(arr[index], parseInt(index))) {
-            res.push(arr[index]);
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            res.push(arr[i]);
         }
     }
     return res;
