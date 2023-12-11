4ea117c2aad2789fc8516be5bb0d1737b8449ad1
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-12-12 10:05:34 +1100

    Time: 49 ms (86.75%), Space: 44.8 MB (12.30%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+    
+    let res: number[] = [];
+    
+    function postorder(node: TreeNode | null) {
+        if (!node) {
+            return res;
+        }
+        
+        postorder(node.left);
+        postorder(node.right);
+        res.push(node.val);
+        
+        return res;
+    }
+    postorder(root);
+    return res;
+};
