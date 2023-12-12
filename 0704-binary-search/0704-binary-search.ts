c1f5d054131aa5899578e81e14eb8a43f3456ba1
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2023-12-12 11:33:31 +1100

    Time: 53 ms (87.11%), Space: 46.3 MB (22.94%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.ts
@@ -0,0 +1,18 @@
+function search(nums: number[], target: number): number {
+    
+    function bs(lo: number, hi: number): number {
+        if (lo > hi) {
+            return -1;
+        }
+        
+        const mid = Math.floor((lo + hi) / 2);
+        if (target > nums[mid]) {
+            return bs(mid + 1, hi);
+        } else if (target < nums[mid]) {
+            return bs(lo, mid - 1);
+        } else {
+            return mid;
+        }
+    }
+    return bs(0, nums.length - 1);
+};
