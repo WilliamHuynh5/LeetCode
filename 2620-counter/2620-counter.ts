9f695b99bcdf2d5a9746eb5bcbf47416f3f05a4e
William Huynh <68536187+WilliamHuynh5@users.noreply.github.com>
2024-03-12 21:41:01 +1100

    Time: 54 ms (65.35%), Space: 51.2 MB (48.37%) - LeetHub

---
+++ 2620-counter/2620-counter.ts
@@ -0,0 +1,14 @@
+function createCounter(n: number): () => number {
+    let i = 0
+    return function() {
+        return n + i++;
+    }
+}
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
